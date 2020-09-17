5a90cfa1e52cb7c05743c599ad1691b2871c404e
Eric Landeis <nightshadedude@gmail.com>
2020-09-16 19:35:46 -0600

    Clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -8,7 +8,6 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        println!("new: hours: {}, min: {}", hours, minutes);
         let temp_min = hours * 60 + minutes;
         let temp_clock = Clock{
             hours: 0,
@@ -18,23 +17,13 @@ impl Clock {
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        println!("add: clock:{}, min: {}", self, minutes);
-
-        let mut temp_min = self.minutes + self.hours + minutes;
-        println!("add minutes:{}", temp_min);
-        let mut temp_hours = 0;
-        let is_negative = if temp_min < 0 { true } else { false };
-        if is_negative {
-            temp_min = temp_min * -1;
+        let mut temp_min = self.minutes + ( self.hours * 60 ) + minutes;
+        while temp_min < 0 {
+            temp_min = temp_min + ( 60 * 24)
         }
-        
-        temp_hours = ((temp_min - (temp_min % 60)) / 60) % 24;
+        temp_min = temp_min % ( 60 * 24);
+        let temp_hours = ((temp_min - (temp_min % 60)) / 60) % 24;
         temp_min = temp_min % 60;
-        
-        if is_negative {
-            temp_hours = temp_hours * -1;
-            temp_min = temp_min * -1;
-        }
 
         Clock{
             hours: temp_hours,
