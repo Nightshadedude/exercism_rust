0a84922fc5b1a7867f9ae557f56d13049fe94568
Eric Landeis <nightshadedude@gmail.com>
2020-10-18 20:01:10 -0600

    sieve complete

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,3 +1,22 @@
-pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
-}
+pub fn primes_up_to(n: u64) -> Vec<u64> {                                            
+    let mut prime_sieve: Vec<u64> = vec![2,3];                                       
+    let mut check_prime = 3;                                                         
+    while prime_sieve.last().unwrap() <= &n && check_prime <= n{                     
+        check_prime += 2;                                                            
+        let mut factor_count = 0;                                                    
+        for i in 0..prime_sieve.len(){                                               
+            if check_prime % prime_sieve[i] == 0 {                                   
+                factor_count += 1;                                                   
+                break;                                                               
+            }                                                                        
+        }                                                                            
+        if factor_count == 0 && check_prime <= n {                                   
+            prime_sieve.push(check_prime);                                           
+        }                                                                            
+    }                                                                                
+    match n {                                                                        
+        _ if n <= 1 => vec![],                                                       
+        2 => vec![2],                                                                
+        _ => prime_sieve,                                                            
+    }                                                                                
+} 
