f0c4cdaca3f0f5e2c304c4de8258afc182f81898
Eric Landeis <nightshadedude@gmail.com>
2020-10-24 21:05:12 -0600

    completed rna transcription

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,64 @@
 #[derive(Debug, PartialEq)]
-pub struct DNA;
+pub struct DNA {
+    dna: String,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct RNA;
+pub struct RNA {
+    rna: String,
+}
 
 impl DNA {
     pub fn new(dna: &str) -> Result<DNA, usize> {
-        unimplemented!("Construct new DNA from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        if dna.len() == 0 { return Err(0usize)}
+        for i in 0..dna.len() {
+            let ch = dna.chars().nth(i).unwrap();
+            match ch {
+                'A' => (),
+                'C' => (),
+                'G' => (),
+                'T' => (),
+                _ => return Err(i),
+            }
+        }
+        Ok(DNA{
+            dna: String::from(dna),
+        })
     }
 
+
     pub fn into_rna(self) -> RNA {
-        unimplemented!("Transform DNA {:?} into corresponding RNA", self);
+        let ret_str = self.dna
+            .chars()
+            .map(|ch| match ch {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => ' ',
+            }).collect::<Vec<char>>()
+            .into_iter()
+            .collect::<String>();
+        RNA::new(&ret_str).unwrap()
     }
 }
 
 impl RNA {
     pub fn new(rna: &str) -> Result<RNA, usize> {
-        unimplemented!("Construct new RNA from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        if rna.len() == 0 { return Err(0usize)}
+        for i in 0..rna.len() {
+            let ch = rna.chars().nth(i).unwrap();
+            match ch {
+                'U' => (),
+                'G' => (),
+                'C' => (),
+                'A' => (),
+                _ => return Err(i),
+            }
+        }
+        Ok(RNA{
+            rna: String::from(rna),
+        })
     }
+
 }
