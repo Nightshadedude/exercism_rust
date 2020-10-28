a6762f2fb6747792cdc5bfe87356a1dd04b92f19
Eric Landeis <nightshadedude@gmail.com>
2020-10-28 17:50:23 -0600

    complete triangle

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,19 +1,44 @@
-pub struct Triangle;
+pub struct Triangle{
+    side: Vec<u64>,
+}
 
 impl Triangle {
-    pub fn build(sides: [u64; 3]) -> Option<Triangle> {
-        unimplemented!("Construct new Triangle from following sides: {:?}. Return None if the sides are invalid.", sides);
+    pub fn build(mut sides: [u64; 3]) -> Option<Triangle> {
+        let mut ret_sides = vec![];
+        let mut sum = 0;
+        sides.sort();
+        for i in 0..sides.len() {
+            if sides[i] <= 0 { return None; }
+            else {
+                if i == 2 && sides[i] >= sum { return None; }
+                sum += sides[i];
+                ret_sides.push(sides[i]);
+            }
+        }
+
+        Some(Triangle{
+            side: ret_sides,
+        })
     }
 
     pub fn is_equilateral(&self) -> bool {
-        unimplemented!("Determine if the Triangle is equilateral.");
+        if self.side[0] == self.side[1] && self.side[1] == self.side[2] { return true; }
+        false
     }
 
     pub fn is_scalene(&self) -> bool {
-        unimplemented!("Determine if the Triangle is scalene.");
+        if self.side[0] != self.side[1]
+            && self.side[1] != self.side[2]
+            && self.side[2] != self.side[0]
+            { return true; }
+        false
     }
 
     pub fn is_isosceles(&self) -> bool {
-        unimplemented!("Determine if the Triangle is isosceles.");
+         if self.side[0] == self.side[1]
+            || self.side[1] == self.side[2]
+            || self.side[2] == self.side[0]
+            { return true; }
+        false
     }
 }
