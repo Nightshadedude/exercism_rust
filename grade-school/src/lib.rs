6be7c105161a6acc91eaa63ff553761db04bc645
Eric Landeis <nightshadedude@gmail.com>
2020-10-31 08:31:05 -0600

    completed grade school

--- grade-school/src/lib.rs
+++ grade-school/src/lib.rs
@@ -1,16 +1,31 @@
-pub struct School {}
+pub struct School {
+    grades: Vec<(u32,Vec<String>)>,
+}
 
 impl School {
     pub fn new() -> School {
-        unimplemented!()
+        School {
+            grades: vec![],
+        }
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+        let mut pushed = false;
+        for i in 0..self.grades.len() {
+            if self.grades[i].0 == grade {
+                self.grades[i].1.push(String::from(student));
+                pushed = true;
+            }
+        }
+        if !pushed {
+            self.grades.push((grade, vec![String::from(student)]));
+        }
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        let mut grades = self.grades.iter().map(|(gr, _)| *gr).collect::<Vec<u32>>();
+        grades.sort();
+        grades
     }
 
     // If grade returned an `Option<&Vec<String>>`,
@@ -18,6 +33,16 @@ impl School {
     // By returning an owned vector instead,
     // the internal implementation is free to use whatever it chooses.
     pub fn grade(&self, grade: u32) -> Option<Vec<String>> {
-        unimplemented!("Return the list of students in {}", grade)
+        let mut students = vec![];
+        for i in 0..self.grades.len() {
+            if self.grades[i].0 == grade {
+                students = self.grades[i].clone().1;
+            }
+        }
+        students.sort();
+        match students.len() {
+            0 => return None,
+            _ => return Some(students),
+        }
     }
 }
