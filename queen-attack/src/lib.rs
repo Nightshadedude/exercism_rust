e5cf178bab8064916624fbdbbc55b38a83c0545f
Eric Landeis <nightshadedude@gmail.com>
2020-11-11 22:05:50 -0700

    completed queen attack

--- queen-attack/src/lib.rs
+++ queen-attack/src/lib.rs
@@ -1,31 +1,41 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    column: i32,
+    row: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        if rank >= 8 || file >= 8 ||
+            rank < 0 || file < 0 { return None; }
+        Some(ChessPosition {
+            column: rank,
+            row: file,
+        })
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Queen {
+            position,
+        }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        if self.position.column == other.position.column ||
+        self.position.row == other.position.row {
+            return true;
+        } else if (self.position.column - other.position.column).abs() ==
+        (self.position.row - other.position.row).abs() {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
