4bdf6f12b379a3f3e884bcf6ba0ad3aa3bc1231c
Eric Landeis <nightshadedude@gmail.com>
2020-11-26 11:19:20 -0700

    different approach - in progress

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -6,47 +6,27 @@ pub enum Error {
 }
 
 pub struct Frame {
-    roll_1: u16,
-    roll_2: Option<u16>,
-}
-
-impl Frame {
-    pub fn new(pins: u16) -> Result<Self, Error> {
-        match pins {
-            0..=9 => {
-                Ok(Frame {
-                    roll_1: pins,
-                    roll_2: None,
-                }),
-            },
-            10 => Ok(Frame {
-                roll_1: pins,
-                roll_2: Some(0),
-            },
-            _ => Err(Error::NotEnoughPinsLeft),
-    }
-
-    pub fn update(&mut self, pins: u16) -> Result<(), Error> {
-        match self.roll_2 {
-            Some(_) => Err(Error::FrameFull),
-            None => {
-                match self.roll_1 + pins {
-                    0..=10 => {
-                        self.roll_2 = Some(pins);
-                        Ok(())
-                    },
-                    _ => Err(Error::NotEnoughPinsLeft),
-                }
-            },
-        }
-    }
+    roll: Vec<u16>,
 }
 
 pub struct BowlingGame {
    frames: Vec<Frame>,
-   ten_frame: Option<u16>,
 }
 
+
+impl Frame {
+    pub fn new(pins: u16) -> Result<Self, Error> {
+        match pins {
+            0..=9 => Ok(Frame {
+                roll: vec![pins],
+            }),
+            10 => Ok(Frame {
+                roll: vec![pins, 0],
+            }),
+            _ => Err(Error::NotEnoughPinsLeft),
+        }
+    }
+ 
 impl BowlingGame {
     pub fn new() -> Self {
         BowlingGame {
@@ -59,36 +39,15 @@ impl BowlingGame {
         let len = self.frames.len();
         match len {
             0 => {
-               match pins {
-                   0..=9 => {
-                       let frame = Frame {
-                           roll_1: pins,
-                           roll_2: None,
-                       };
-                       self.frames.push(frame);
-                       Ok(())
-                   },
-                   10 => {
-                       let frame = Frame {
-                           roll_1: pins,
-                           roll_2: Some(0),
-                       };
-                       self.frames.push(frame);
-                       Ok(())
-                   },
-                   _ => Err(Error::NotEnoughPinsLeft),
-               }
+                let frame = Frame::new(pins);
+                match frame {
+                }
             },
-            1..=9 {
-                match self.frames.last().unwrap().update(pins) {
-                    Ok => Ok(()),
-                    Err(Error::FrameFull) => {
-                        match pins {
-                            0..=9
+            1..=9 => {
             },
             10 => {
             },
-            _ => Err(GameComplete),
+            _ => Err(Error::GameComplete),
         }
     }
 
